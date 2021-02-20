2def929e8eb5a148a5acdce76219ad8e110c66d6
ankitt01 <ankitrawat0102@gmail.com>
2021-02-20 21:58:29 +0530

    added cursor animations

--- script.js
+++ script.js
@@ -63,4 +63,32 @@ function animateSlides() {
     });
 }
 
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
+function cursor(e) {
+    
+    mouse.style.top = e.pageY + "px";
+    mouse.style.left = e.pageX + "px";
+}
+function activeCursor(e) {
+    const item = e.target;
+    if(item.id == 'logo' || item.classList.contains('burger')) {
+        mouse.classList.add('nav-active');
+    }else {
+        mouse.classList.remove('nav-active');
+    }
+    if(item.classList.contains('explore')) {
+        mouse.classList.add('explore-active');
+        gsap.to(".title-swipe", 1, {y:"0%"})
+
+        mouseTxt.innerText = 'Tap!';
+    }else {
+        mouse.classList.remove('explore-active');
+        mouseTxt.innerText = '';
+        gsap.to(".title-swipe", 1, {y:"100%"})
+    }
+}
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mousemove", activeCursor);
+
 animateSlides();
