a1c07c03ed101ed3c08d7f99c070d7a8ddb84893
ankitt01 <ankitrawat0102@gmail.com>
2021-02-20 21:34:28 +0530

    added scroll animation using ScrollMagic

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let controller;
 let slideScene;
+let pageScene;
 
 function animateSlides() {
     controller = new ScrollMagic.Controller();    
@@ -8,7 +9,7 @@ function animateSlides() {
     const nav = document.querySelector('.nav-header');
 
     //loop over each slide
-    sliders.forEach(slide => {
+    sliders.forEach((slide,index,slides) => {
         const revealImg = slide.querySelector('.reveal-img');
         const img = slide.querySelector('img');
         const revealText = slide.querySelector('.reveal-text');
@@ -21,6 +22,44 @@ function animateSlides() {
         slideTl.fromTo(img, {scale:1.5}, {scale:1}, '-=1');
         slideTl.fromTo(revealText, {x:"0%"},{x:"100%"}, '-=0.75');
         slideTl.fromTo(nav, {y:"-100%"},{y:"0%"}, '-=0.6');
+
+        //Create Scene
+        slideScene = new ScrollMagic.Scene( {
+            triggerElement: slide,
+            triggerHook: 0.25,
+            reverse:false
+        })
+        .setTween(slideTl)
+        .addIndicators( {
+            colorStart: "white",
+            colorTrigger: "white",
+            name: "slide"
+        })
+        .addTo(controller)
+
+        //New Animation
+        const pageTl = gsap.timeline();
+        let nextSlide = slides.length - 1 === index ? 'end' : slides[index+1];
+        pageTl.fromTo(nextSlide, {y:'0%'}, {y:'50%'})
+        pageTl.fromTo(slide, {opacity:1, scale:1}, {opacity: 0, scale:0.5})
+        pageTl.fromTo(nextSlide, {y:'50%'}, {y:'0%'}, '-=0.5')
+
+        //Create new Scene
+        pageScene = new ScrollMagic.Scene( {
+            triggerElement: slide,
+            duration: '100%',
+            triggerHook: 0
+        })
+        .addIndicators({
+            colorStart: "white",
+            colorTrigger: "white",
+            name: "page",
+            indent: 200
+        })
+        .setPin(slide,{pushFollowers:false})
+        .setTween(pageTl)
+        .addTo(controller)
+
     });
 }
 
