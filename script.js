704aa6e4ff5c6464bdbdf6dc7e8d48a355df0b81
ankitt01 <ankitrawat0102@gmail.com>
2021-02-20 22:13:28 +0530

    Burger and navigation animation

--- script.js
+++ script.js
@@ -63,8 +63,9 @@ function animateSlides() {
     });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseTxt = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
+const mouse = document.querySelector(".cursor");
+const mouseTxt = mouse.querySelector("span");
 function cursor(e) {
     
     mouse.style.top = e.pageY + "px";
@@ -88,6 +89,27 @@ function activeCursor(e) {
         gsap.to(".title-swipe", 1, {y:"100%"})
     }
 }
+
+function navToggle(e) {
+    if(!e.target.classList.contains('active')) {
+        e.target.classList.add('active');
+        gsap.to('.line1', 0.5, {rotate: "45", y: 5, background: 'black'});
+        gsap.to('.line2', 0.5, {rotate: "-45", y: -5, background: 'black'});
+        gsap.to('#logo', 1, {color: 'black'})
+        gsap.to('.nav-bar', 1, {clipPath: 'circle(2500px at 100% -10%'})
+        document.body.classList.add("hide");
+    }else {
+        e.target.classList.remove('active');
+        gsap.to('.line1', 0.5, {rotate: "0", y: 0, background: 'white'});
+        gsap.to('.line2', 0.5, {rotate: "0", y: 0, background: 'white'});
+        gsap.to('#logo', 1, {color: 'white'})
+        gsap.to('.nav-bar', 1, {clipPath: 'circle(50px at 100% -10%'})
+        document.body.classList.remove("hide");
+    }
+}
+
+
+burger.addEventListener('click', navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mousemove", activeCursor);
 
